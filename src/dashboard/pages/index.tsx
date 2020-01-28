50f006df8f6af525a3604dac7ddaca9883d93fd1
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-01-28 02:53:28 -0500

    Revert "removed typescript"
    This reverts commit 4d1bb12b0bf6f3ec9a8e08ed4f3deb50ffac262e.

--- src/dashboard/pages/index.tsx
+++ src/dashboard/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Index() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+          Next.js with TypeScript example
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
--- src/dashboard/src/theme.tsx
+++ src/dashboard/src/theme.tsx
@@ -1,5 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import red from '@material-ui/core/colors/red';
 
 // Create a theme instance.
 const theme = createMuiTheme({
