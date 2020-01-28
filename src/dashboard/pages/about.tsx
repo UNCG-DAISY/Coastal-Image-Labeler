ec0ccc58d7ef3c01abb10ab526648b967afdc634
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-01-28 02:53:28 -0500

    Revert "removed typescript"
    This reverts commit 4d1bb12b0bf6f3ec9a8e08ed4f3deb50ffac262e.

--- src/dashboard/pages/about.tsx
+++ src/dashboard/pages/about.tsx
@@ -3,7 +3,6 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import MuiLink from '@material-ui/core/Link';
-import Button from '@material-ui/core/Button';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 
@@ -25,11 +24,9 @@ export default function About() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+          Next.js with TypeScript example
         </Typography>
-        <Button variant="contained" color="primary" component={Link} naked href="/">
-          Go to the main page
-        </Button>
+        <Link href="/">Go to the main page</Link>
         <ProTip />
         <Copyright />
       </Box>
