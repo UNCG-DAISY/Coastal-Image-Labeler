2e6cd5564f7f89c84df78463589b37f39dcef75a
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-03-23 14:28:53 -0400

    added RBAC method

--- src/server/_src/middleware/v1/auth.ts
+++ src/server/_src/middleware/v1/auth.ts
@@ -6,6 +6,8 @@
 import {asyncHandler} from './async'  
 import {ErrorResponse} from '../../utils/v1/errorResponse'
 import { Request,Response,NextFunction } from "express"
+import {rbacJson,UserDocument} from '../../index'
+import {UserModel} from '../../models/User'
 
 //protect routes
 
@@ -44,7 +46,49 @@ const authorize = (...roles:string[]) => {
     }
 }
 
+const RBAC = async (user:UserDocument,roles:rbacJson) => {
+    let userRoles = user.roleNames
+
+    // if(userRoles) {
+    //     userRoles =  await UserModel.findOne(user._id).populate('roleData')
+    // }
+
+    let {all:mustHaveRoles,one:atleastOneRoles} = roles
+
+    let userRolesSet = new Set(userRoles)
+
+    //if there are roles that must have
+    if(mustHaveRoles.length > 0) {
+        let mustHaveRolesSet = new Set(mustHaveRoles)
+    
+
+        let allRolesIntersection = new Set(
+            [...mustHaveRolesSet].filter(x => userRolesSet.has(x)));
+        
+        //If doenst have all must have roles
+        if(allRolesIntersection.size !== mustHaveRolesSet.size) {
+            return false
+        }
+    }
+   
+    //if ther are roles where only need one
+    if(atleastOneRoles.length > 0 ) {
+        let atleastOneRolesSet = new Set(atleastOneRoles)
+        let atleastOneRolesIntersection = new Set(
+            [...atleastOneRolesSet].filter(x => userRolesSet.has(x)));
+    
+        //if no roles have from atleast need one
+        if(atleastOneRolesIntersection.size<1) {
+            return false
+        }
+    }
+    
+    //passed all checks
+    return true
+}
+
 export {
     protect,
-    authorize
+    authorize,
+    RBAC
 }
