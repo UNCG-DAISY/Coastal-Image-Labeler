51137caedca0992d5f33e21ee7ddf03ac5770e1e
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-05-06 02:17:06 -0400

    Finished buttons

--- src/server/_site/components/taggingForm/Form.js
+++ src/server/_site/components/taggingForm/Form.js
@@ -1,23 +1,20 @@
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import * as colors from '@material-ui/core/colors/';
+
 
 import RadioButton from '../radio/RadioButton'
 import Radio from '@material-ui/core/Radio';
 
 import RadioButtonGroup from '../radio/RadioButtonGroup'
+
+import RadioButtonQuestions from './RadioButtonQuestions'
 export default class Form extends React.Component {
     state = {
         ...this.props.tags
     }
-    updateRadio(params){
-        this.setState({
-            [params.key]:params.value
-        })
    
-    }
 
-    testfnc = (params) => {
+    updateRadio = (params) => {
         this.setState({
             [params.key]:params.value
         })
@@ -29,28 +26,13 @@ export default class Form extends React.Component {
                 <Typography variant="h6" color="textSecondary" component="p">
                     Image Classification Categories
                 </Typography>
-        
-                <RadioButtonGroup
-                 onChange={this.testfnc}
-                 style={{color:colors.green[500]}}
-                 title={`Development Type`}
-                 keyValue={'devType'}
-                 state={this.state.devType}
-                 buttons={[
-                    {
-                        value:"0",
-                        control: <Radio color="primary" />,
-                        label: "Undeveloped",
-                        labelPlacement:"end"
-                    },
-                    {
-                        value:'1',
-                        control: <Radio color="primary" />,
-                        label: "Developed",
-                        labelPlacement:"end"
+                <RadioButtonQuestions
+                    state = {
+                        this.state.devType
                     }
-                ]}
+                    updateFunction = {this.updateRadio}
                 />
+                
             </CardContent>
         )
     }
