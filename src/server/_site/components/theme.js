8db2b8351e744a843ba39ad026bdc740e71b7747
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-05-06 04:55:08 -0400

    font

--- src/server/_site/components/theme.js
+++ src/server/_site/components/theme.js
@@ -5,6 +5,24 @@ import { red } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+    //fontSize: 24,
+    fontFamily: [
+      // '-apple-system',
+      // 'BlinkMacSystemFont',
+      // '"Segoe UI"',
+      // 'Roboto',
+      // '"Helvetica Neue"',
+      // 'Arial',
+      // 'sans-serif',
+      // '"Apple Color Emoji"',
+      // '"Segoe UI Emoji"',
+      // '"Segoe UI Symbol"',
+      'Roboto Mono','monospace'
+    ].join(','),
+  },
   palette: {
     type:'dark',
     primary: {
