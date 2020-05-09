2e98344ce797e87370b5b3bbfc84679f46d6fca9
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-05-09 00:14:26 -0400

    removed archive code folder

--- src/server/_archiveCode/steppers/handleWaterImage.js
+++
@@ -1,7 +0,0 @@
-import initalTagState from './initalTagState'
-
-export default function handleWaterImage() {
-    let tag = initalTagState
-    tag.water = 1
-    alert('This is water image');
-}
--- src/server/_archiveCode/steppers/isCheckboxAllowedToContinue.js
+++
@@ -1,9 +0,0 @@
-export default function isCheckboxAllowedToContinue(states,required = 0) {
-    const selected = Object.keys(states).filter((element) =>{
-        if(states[element] > 0) {
-        return true
-        }
-    })
-
-    return selected.length >=required? 1:-1
-}
