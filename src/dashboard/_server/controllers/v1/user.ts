b1a611314b69e841fafe177efda56ab5696e12e2
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-05-19 23:10:31 -0400

    removed outdated codez

--- src/dashboard/_server/controllers/v1/user.ts
+++ src/dashboard/_server/controllers/v1/user.ts
@@ -11,53 +11,6 @@ import axios from 'axios'
 import {ArchiveModel} from '../../models/Archive'
 import {ImageModel} from '../../models/Image'
 
-
-// /**
-//  * @desc        Gets all roles of a user
-//  * @route       GET /api/v1/users/isUser
-//  * @access      Public
-//  * @returns     yes
-//  */
-// const getUserRoles = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    
-//     const userId = req?.body?.id
-
-//     //If no user id sent
-//     if(!userId) {
-//         return next(new ErrorResponse(`Please send a userId`,400))
-//     }
-    
-//     //Call the auth0 api to get uses of the user
-//     const options:any = {
-//         method: 'GET',
-//         url: `https://${process.env.AUTH0_DOMAIN}/api/v2/users/${userId}/roles`,
-//         headers:{
-//             authorization: `Bearer ${global.MANGAGEMENT_TOKEN}`
-//         }
-//     };
-    
-//     //Call auth0 api for user roles
-//     const role_data:[any] = (await axios.get(options.url,options)).data
-    
-//     let roles = []
-
-//     //Since array of roles,just get the important stuff
-//     role_data.forEach(role => {
-//         roles.push({
-//             role:role.name,
-//             role_description:role.description
-//         })
-//     });
-
-//     res.status(200).json({
-//         success:true,
-//         data:{
-//            roles:roles
-//         }
-//     })
-   
-// })
-
 /**
  * @desc        Gets a user by id
  * @route       GET /api/v1/users/findUser
@@ -102,52 +55,6 @@ const findUser = asyncHandler(async (req: Request, res: Response, next: NextFunc
     }
     
 })
-
-// /**
-//  * @desc        Given a user id, and an array of roles, check if user has those roles
-//  * @route       GET /api/v1/users/:id
-//  * @access      Public
-//  * @returns     yes
-//  */
-// const checkUserRoles = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    
-//     //Get user and roles
-//     const user  = await UserModel.findById(req?.params?.id).populate('roleData')
-//     const userRoles = user.roleNames
-
-//     //get allowed roles
-//     const allowedRoles = req?.body?.allowedRoles ?? []
-
-//     //Make allowed roles and user roles into set, do intersection and see whats result
-//     const user_roles_set:Set<string> = new Set(userRoles)
-//     const roles_set:Set<string> = new Set(allowedRoles)
-
-//     const role_intersection:Set<string> = new Set(
-//         [...user_roles_set].filter(x => roles_set.has(x)));
-
-//     //If theres no common roles, denied
-//     if(role_intersection.size === 0) {
-//         res.status(200).json({
-//             success:true,
-//             data:{
-//                 message: 'Denied',
-//                 allowed:false
-//             }
-//         }) 
-//     }
-//     else {
-//         //If theres atleast 1 common role, allowed
-//         res.status(200).json({
-//             success:true,
-//             data:{
-//                 message: 'Allowed',
-//                 allowed:true
-//             }
-//         }) 
-//     }
-    
-    
-// })
 
 //Perhaps this should be only allowed by logged in users
 /**
