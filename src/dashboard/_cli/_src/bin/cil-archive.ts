8f711066f8b683afc8af4542ef6f61b929feccd1
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-05-25 20:23:47 -0400

    added specific archive adding and added image adding
    close #50

--- src/dashboard/_cli/_src/bin/cil-archive.ts
+++ src/dashboard/_cli/_src/bin/cil-archive.ts
@@ -10,43 +10,14 @@ import MongoConnection from '../lib/MongoConnection'
 program
     .command('add')
     .description('Path to the archive data')
-    .option('-p, --path <type>','Give path to the archives',undefined)
-    .option('-a, --all','Add all directories of --path as archives',false)
-    .option('-i, --images','Add all images of the archive',false)
-    .option('-d, --dev','dev mode',false)
+    .option('-p, --path <type>','Give path to the JSON file',undefined)
     .action(async (cmd) => {
         const {
             path,
-            all,
-            images,
-            dev
         } = cmd
 
-        //make sure a path is given
-        if(!path) return console.log('Please provide a path')
+        archive.createSpecificArchive({path:path})
 
-        //connect to db
-        const uriManager = new UriManager();
-        const mongoConnection = new MongoConnection(uriManager.getKey())
-        await mongoConnection.connect()
-
-        //TEST COMMAND
-            //await CatalogModel.deleteMany({})
-            if(dev) {
-                await ArchiveModel.deleteMany({})
-                await ImageModel.deleteMany({})
-            }
-
-        //first check to see if the catalog exists
-        const pathArray = path.split("\\")
-        const catalogName = pathArray[pathArray.length-1]
-
-        console.log(catalogName,' ---- TEST')
-        console.log(pathArray)
-        //const catalog = await CatalogModel.findOne({name:catalogName})
-        //const archiveResult = await archive.addArchives(path,catalog._id,{all,images})
-
-        await mongoConnection.close()
     })
 
 program.parse(process.argv)
