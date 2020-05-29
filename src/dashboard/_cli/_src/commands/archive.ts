d06c69de8e51aecdca95572f22cac79b2db3cb5d
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-05-29 07:12:26 -0400

    uncomment

--- src/dashboard/_cli/_src/commands/archive.ts
+++ src/dashboard/_cli/_src/commands/archive.ts
@@ -14,116 +14,116 @@ import image from './image'
 import fs from 'fs'
 
 const archive = {
-    // async createArchives(options) {
-    //     const {
-    //         path,
-    //         catalogId,
-    //         allImages
-    //     } = options
+    async createArchives(options) {
+        const {
+            path,
+            catalogId,
+            allImages
+        } = options
 
-    //     if(!path) {return {error:true,errorMessage:'no path passed'}}
-    //     if(!catalogId) {return {error:true,errorMessage:'no catalog id passed'}}
+        if(!path) {return {error:true,errorMessage:'no path passed'}}
+        if(!catalogId) {return {error:true,errorMessage:'no catalog id passed'}}
 
-    //     //get archives of catalog
-    //     const dirs = getDirectories(path)
-    //     colorize.info(`Archives are ${dirs.toString()}`)
+        //get archives of catalog
+        const dirs = getDirectories(path)
+        colorize.info(`Archives are ${dirs.toString()}`)
 
-    //     //check catalogId to see if valid archive
-    //     const catalog = CatalogModel.findById(catalogId)
-    //     if(!catalog) {return {error:true,message:`No catalog found with _id ${catalogId}`}}
+        //check catalogId to see if valid archive
+        const catalog = CatalogModel.findById(catalogId)
+        if(!catalog) {return {error:true,message:`No catalog found with _id ${catalogId}`}}
 
-    //     await Promise.all(dirs.map(async(archiveName,index)=> {
-    //         const archivePath = `/${archiveName}/jpgs`
+        await Promise.all(dirs.map(async(archiveName,index)=> {
+            const archivePath = `/${archiveName}/jpgs`
 
-    //         //check if archive exists
-    //         const existingArchive = await ArchiveModel.find({ 
-    //             $or: [ 
-    //                 { name: archiveName }, 
-    //                 { path: archivePath } 
-    //             ] 
-    //         })
+            //check if archive exists
+            const existingArchive = await ArchiveModel.find({ 
+                $or: [ 
+                    { name: archiveName }, 
+                    { path: archivePath } 
+                ] 
+            })
             
-    //         //if exists
-    //         if(existingArchive.length>0) {
-    //             return colorize.warning(`Archive ${archiveName} exists`)
-    //         }
+            //if exists
+            if(existingArchive.length>0) {
+                return colorize.warning(`Archive ${archiveName} exists`)
+            }
 
-    //         //if not make one
-    //         const archiveEntry = await ArchiveModel.create({
-    //             "dateAdded":Date.now(),
-    //             "name" : archiveName,
-    //             "path" : archivePath,
-    //             "taggable": true,
-    //             "catalog": catalogId,
-    //         })
+            //if not make one
+            const archiveEntry = await ArchiveModel.create({
+                "dateAdded":Date.now(),
+                "name" : archiveName,
+                "path" : archivePath,
+                "taggable": true,
+                "catalog": catalogId,
+            })
 
-    //         if(allImages) {
-    //             await image.addImages(archiveEntry._id)
-    //         }
-    //     }))
-    // },
+            if(allImages) {
+                await image.addImages(archiveEntry._id)
+            }
+        }))
+    },
 
-    // async createSpecificArchive(options) {
-    //     const {
-    //         path 
-    //     } = options
-    //     if(!path) {return {error:true,errorMessage:'no path passed'}}
+    async createSpecificArchive(options) {
+        const {
+            path 
+        } = options
+        if(!path) {return {error:true,errorMessage:'no path passed'}}
 
-    //     const file = JSON.parse(fs.readFileSync(path, 'utf8'));
+        const file = JSON.parse(fs.readFileSync(path, 'utf8'));
         
-    //     //connect to db
-    //     const uriManager = new UriManager();
-    //     const mongoConnection = new MongoConnection(uriManager.getKey())
-    //     await mongoConnection.connect()
+        //connect to db
+        const uriManager = new UriManager();
+        const mongoConnection = new MongoConnection(uriManager.getKey())
+        await mongoConnection.connect()
 
-    //     const archives = file.archives
-    //     //console.log(archives)
+        const archives = file.archives
+        //console.log(archives)
 
-    //     await Promise.all(archives.map(async (archive,index)=>{
+        await Promise.all(archives.map(async (archive,index)=>{
 
-    //         //get catalog and archive name
-    //         const pathArray = archive.path.split("/")
-    //         const archiveName = pathArray[pathArray.length-1]
-    //         const catalogName = pathArray[pathArray.length-2]
+            //get catalog and archive name
+            const pathArray = archive.path.split("/")
+            const archiveName = pathArray[pathArray.length-1]
+            const catalogName = pathArray[pathArray.length-2]
 
-    //         //see if archive exists
-    //         const existingArchive = await ArchiveModel.find({ 
-    //             $or: [ 
-    //                 { path: `/${archiveName}` },
-    //                 { name: archiveName }
-    //             ] 
-    //         })
-    //         if(existingArchive.length>0) {
-    //             return colorize.warning(`Archive ${archiveName} already exists`)
-    //         }
+            //see if archive exists
+            const existingArchive = await ArchiveModel.find({ 
+                $or: [ 
+                    { path: `/${archiveName}` },
+                    { name: archiveName }
+                ] 
+            })
+            if(existingArchive.length>0) {
+                return colorize.warning(`Archive ${archiveName} already exists`)
+            }
 
-    //         //see if catalog exists
-    //         const existingCatalog = await CatalogModel.find({ name:catalogName})
-    //         if(existingCatalog.length == 0) {
-    //             return colorize.warning(`Catalog for ${archiveName} does NOT exists`)
-    //         }
+            //see if catalog exists
+            const existingCatalog = await CatalogModel.find({ name:catalogName})
+            if(existingCatalog.length == 0) {
+                return colorize.warning(`Catalog for ${archiveName} does NOT exists`)
+            }
 
-    //         //enter into db
-    //         const archiveEntry = await ArchiveModel.create({
-    //             "dateAdded":Date.now(),
-    //             "name" : archiveName,
-    //             "path" : `/${archiveName}/jpgs`,
-    //             "taggable": archive.taggable,
-    //             "catalog": existingCatalog[0]._id,
-    //         })
-    //         colorize.log(`Archive ${archiveEntry.name} created`)
+            //enter into db
+            const archiveEntry = await ArchiveModel.create({
+                "dateAdded":Date.now(),
+                "name" : archiveName,
+                "path" : `/${archiveName}/jpgs`,
+                "taggable": archive.taggable,
+                "catalog": existingCatalog[0]._id,
+            })
+            colorize.log(`Archive ${archiveEntry.name} created`)
             
-    //         //if create all images flag is given
-    //         if(archive.createAllImages) {
-    //             await image.addImages(archiveEntry._id)
-    //         }
+            //if create all images flag is given
+            if(archive.createAllImages) {
+                await image.addImages(archiveEntry._id)
+            }
             
 
-    //     }))
+        }))
 
-    //     //close connection to db
-    //     await mongoConnection.close()
-    // },
+        //close connection to db
+        await mongoConnection.close()
+    },
 
     async createArchive(options) {
         const {
