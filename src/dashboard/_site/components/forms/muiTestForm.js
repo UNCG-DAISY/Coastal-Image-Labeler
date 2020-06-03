80a00c76df0be8cfb17e8243dd8d24801a4b8b57
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-06-03 07:05:24 -0400

    mui radios

---
+++ src/dashboard/_site/components/forms/muiTestForm.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import {
+    Button,
+    TextField,
+    Container,
+    CssBaseline,
+    Grid,
+    Typography,
+    RadioGroup,
+    FormControlLabel,
+    FormControl,
+    Radio,
+    makeStyles,
+    FormLabel,
+    FormGroup,
+    Checkbox,
+} from "@material-ui/core";
+import theme from '../theme';
+import CardContent from '@material-ui/core/CardContent';
+
+export default function MuiTestForm() {
+    const { register, handleSubmit, errors, watch, getValues, control  } = useForm({
+        defaultValues:{
+            myRadios:""
+        }
+    });
+    const onSubmit = data => {
+        console.log('submit',data)
+    };
+
+    return (
+        <React.Fragment>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <div>  
+                    <React.Fragment>
+                        <FormLabel component="legend">BIG TEST</FormLabel>
+                        <Controller
+                            as={
+                                <RadioGroup aria-label="Pizza type" row>
+                                    {["MuiR1","MuiR2","MuiR3"].map((pizza) => (
+                                        <FormControlLabel
+                                            value={pizza}
+                                            control={<Radio />}
+                                            label={pizza}
+                                            key={pizza}
+                                        />
+                                    ))}
+                                </RadioGroup>
+                            }
+                            name="myRadios"
+                            control={control}
+                            rules={{ required: true }}
+                        />
+                    </React.Fragment>
+                    <br/>
+                    {errors.myRadios && "Error myRadios"}
+                </div>
+
+                <div>
+                    <input 
+                        type="checkbox" placeholder="myCheckbox" 
+                        name="myCheckbox" value="C1" 
+                        ref={
+                            register({
+                                required: true,
+                                validate:value=>{
+                                    //console.log( value,getValues().myCheckbox.length)
+                                    const lengthVal = getValues().myCheckbox.length
+                                    return (
+                                       (lengthVal>=2 && lengthVal<=3) ? true:false
+                                    )
+                                }
+                            })
+                        } 
+                    />
+                    <label>C1</label>
+                    <input type="checkbox" placeholder="myCheckbox" name="myCheckbox" value="C2" ref={register({required: true,min:2})} />
+                    <label>C2</label>
+                    <input type="checkbox" placeholder="myCheckbox" name="myCheckbox" value="C3" ref={register({required: true,min:2})} />
+                    <label>C3</label>
+                    <input type="checkbox" placeholder="myCheckbox" name="myCheckbox" value="C4" ref={register({required: true,min:2})} />
+                    <label>C4</label>
+                    <br/>
+                    {errors.myCheckbox && "Error myCheckbox"}
+                </div>
+
+                <br/>
+                <input type="submit" />
+            </form>
+            {JSON.stringify(watch())}
+        </React.Fragment>
+    );
+}
+
+
