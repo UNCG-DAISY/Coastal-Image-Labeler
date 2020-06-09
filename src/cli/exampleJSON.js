64a2b53ad85bb6ba72dc807567cc22490667cb37
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-06-09 11:34:38 +0000

    VM changes

---
+++ src/cli/exampleJSON.js
@@ -0,0 +1,38 @@
+{
+    "compressedFolder":"/home/shahnafis/compressed",
+    "catalogs":[
+        // {
+        //     "path":"/datadrive/archives/Florence",
+        //     "name":"Florence",
+        //     "taggable":true,
+        //     "year":2018,
+        //     "link":"https://en.wikipedia.org/wiki/Hurricane_Florence",
+        //     "description":"Hurricane Florence was a powerful and long-lived Cape Verde hurricane that caused catastrophic damage in the Carolinas in September 2018, primarily as a result of freshwater flooding due to torrential rain. The sixth named storm, third hurricane, and the first major hurricane of the 2018 Atlantic hurricane season, Florence originated from a strong tropical wave that emerged off the west coast of Africa on August 30, 2018.",
+        //     "createAllArchives":true,
+        //     "createAllImages":true,
+        //     "questionSet":"5ed82a588896022158fa0852"
+        // },   
+        {
+            "path":"/datadrive/archives/Dorian",
+            "name":"Dorian",
+            "taggable":true,
+            "year":2019,
+            "link":"https://en.wikipedia.org/wiki/Hurricane_Dorian",
+            "description":"Hurricane Dorian was an extremely powerful and devastating Category 5 Atlantic hurricane, that became the most intense tropical cyclone on record to strike the Bahamas, and is regarded as the worst natural disaster in the country's history.",
+            "createAllArchives":true,
+            "createAllImages":true,
+            "questionSet":"5ed82a588896022158fa0852"
+        },
+        {
+            "path":"/datadrive/archives/Michael",
+            "name":"Michael",
+            "taggable":true,
+            "year":2018,
+            "link":"https://en.wikipedia.org/wiki/Hurricane_Michael",
+            "description":"Hurricane Michael was a very powerful and destructive tropical cyclone that became the first Category 5 hurricane to strike the contiguous United States since Andrew in 1992. In addition, it was the third-most intense Atlantic hurricane to make landfall in the contiguous United States in terms of pressure, behind the 1935 Labor Day hurricane and Hurricane Camille in 1969.",
+            "createAllArchives":true,
+            "createAllImages":true,
+            "questionSet":"5ed82a588896022158fa0852"
+        }      
+    ]
+}
