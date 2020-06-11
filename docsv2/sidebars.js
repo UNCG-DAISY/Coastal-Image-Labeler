506a7f492995b425ed6aa56b1592fc80cfdb5434
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-06-11 00:14:12 -0400

    updates

--- docsv2/sidebars.js
+++ docsv2/sidebars.js
@@ -1,6 +1,64 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  docs: [
+    {
+      type: 'category',
+      label: 'Coastal Image Labeler',
+      items: [
+        'indexDoc',
+        'code_of_conduct',
+        // 'code_documentation/codeDocOverview',
+        // 'user_documentation/userDocOverview'
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Code Documentation',
+      items: [
+        "code_documentation/codeDocOverview",
+        "code_documentation/project_structure",
+        "code_documentation/auth0",
+        "code_documentation/data_exporting",
+        "code_documentation/maintenance",
+        "code_documentation/packages",
+        
+      ],
+    },
+    {
+      type: 'category',
+      label: 'User Documentation',
+      items: [
+        "user_documentation/userDocOverview",
+        "user_documentation/imageTaggingProcess",
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Question Sets',
+      items: [
+        "question_sets/questionSetDocOverview",
+        "question_sets/duneQuestionSet",
+        "question_sets/stormQuestionSet",
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Other',
+      items: [
+        "other/styleGuide",
+        "other/mdx"
+      ],
+    }
+  ],
+  // codeDoc: [
+    
+  // ],
+  // userDoc:[
+  //   {
+  //     type: 'category',
+  //     label: 'User Documentation',
+  //     items: [
+  //       "user_documentation/userDocOverview",
+  //     ],
+  //   },
+  // ]
 };
