17cbcac0749dd19ce1ace5809d80a8e8f7a8fc9c
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-06-16 19:51:31 +0000

    vm changes

--- src/cli/compress_images_path.sh
+++ src/cli/compress_images_path.sh
@@ -15,8 +15,13 @@ for archive in $root_folder/*; do
         echo $archive
         archiveName=$(basename "$archive") 
         archivePath=$archive/jpgs/*.jpg
-        archivePathCompressed=$compressed_folder/$archiveName/jpgs
 
+        archivePathCompressed=$compressed_folder/$archiveName
+        [ ! -d $archivePathCompressed ] && mkdir $archivePathCompressed
+        archivePathCompressed=$compressed_folder/$archiveName/jpgs
+        [ ! -d $archivePathCompressed ] && mkdir $archivePathCompressed
+        
+        
         for image in $archivePath; do
             imageName=$(basename "$image") 
             # echo $image
