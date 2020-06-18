1e05bb11a4e8ec54006b0ffe235b606445c49ad0
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-06-18 13:33:40 +0000

    attempt to have forever be a bash script

--- src/dashboard/forever_start.sh
+++ src/dashboard/forever_start.sh
@@ -1,7 +1,17 @@
-log_folder=/home/shahnafis/forever_logs
-log_name=log
-log_error=error
-log_out=out
+# log_folder=/home/shahnafis/forever_logs
+# log_name=forever_log
+# log_error=forever_error
+# log_out=forever_out
 
-var=$(date +"%FORMAT_STRING")
-now=$(date +"%m_%d_%Y")
+# var=$(date +"%FORMAT_STRING")
+# now=_$(date +"%m_%d_%Y-%H.%M.%S")
+
+# path_log=$log_folder/$log_name$now 
+# path_error=$log_folder/$log_error$now 
+# path_out=$log_folder/$log_out$now 
+
+# sudo FOREVER_ROOT=$log_folder NODE_ENV=production forever start -e $path_error -l $path_log -o $path_out ./_dist/server.js 
+echo hello
+sudo forever list
+# cd /home/shahnafis/GitHub/Coastal-Image-Labeler/src/dashboard
+# NODE_ENV=production  pm2 start ./_dist/server.js
