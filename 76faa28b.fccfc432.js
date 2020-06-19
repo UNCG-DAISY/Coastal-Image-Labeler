0fc05d017505793de3b918ae61a2ba3edd66a734
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-06-19 08:19:21 -0400

    Deploy website - based on c90aa72a37dc514c0c494bce2df687f189315b5e

--- 76faa28b.fccfc432.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{123:function(a){a.exports=JSON.parse('{"allTagsPath":"/Coastal-Image-Labeler/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/Coastal-Image-Labeler/blog/tags/docusaurus"}')}}]);
---
+++ d2b1abd9.5435cf98.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{138:function(a){a.exports=JSON.parse('{"allTagsPath":"/Coastal-Image-Labeler/blog/tags","slug":"resource","name":"resource","count":1,"permalink":"/Coastal-Image-Labeler/blog/tags/resource"}')}}]);
