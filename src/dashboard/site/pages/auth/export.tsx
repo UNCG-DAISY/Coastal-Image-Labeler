70164c588be1455f7a0af8d16cae8cc66f123158
Shah Nafis Rafique <sarafiqu@uncg.edu>
2020-08-26 15:37:41 -0400

    some setup for tag export

--- src/dashboard/site/pages/auth/export.tsx
+++ src/dashboard/site/pages/auth/export.tsx
@@ -7,6 +7,9 @@ import { getUserDB } from '../../components/API/post/getUserDB'
 import { determineNavItems } from '../../components/Utils/Auth/determineNavItems'
 import { ResumeTaggingDataCatalog, UserProp } from '../../../interfaces'
 
+import JSONPretty from 'react-json-pretty'
+import { theme, customColors } from '../../components/theme'
+import { ViewImage } from '../../components/Button/premadeButtons'
 interface Props {
   user: UserProp
   resumeTableData: ResumeTaggingDataCatalog[]
@@ -31,14 +34,35 @@ export const Home = (props: Props): JSX.Element => {
         navItems={determineNavItems(user)}
         drawer
       >
-        exportTags
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <ViewImage variant="contained">Export user tags</ViewImage>
+          {user.data.roles.includes('admin') && (
+            <ViewImage variant="contained">Export all tags</ViewImage>
+          )}
+        </div>
+        <JSONPretty
+          id="json-pretty"
+          data={user}
+          style={{
+            fontSize: '2.0em',
+            backgroundColor: `${theme.palette.background.default}`,
+            paddingLeft: '0.1em',
+          }}
+          theme={{
+            main: `color:#ffffff;background:${theme.palette.background.default}:#;overflow:auto;`,
+            error: 'color:#ffffff;background:#272822;overflow:auto;',
+            key: `color:${theme.palette.primary.light};`,
+            string: `color:${theme.palette.secondary.light};`,
+            value: `color:${customColors.orange};`,
+            boolean: `color:${customColors.purple};`,
+          }}
+        />
       </Layout>
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
-  // const t1 = performance.now()
   //Add user data from db
   const user: any = getSession(context.req)
   user.data =
@@ -46,6 +70,7 @@ export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
       cookie: context?.req?.headers?.cookie,
       res: context.res,
     })) ?? {}
+
   //check if valid
   if (Object.keys(user.data).length === 0) {
     return {
@@ -56,14 +81,10 @@ export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
     }
   }
 
-  // const t2 = performance.now()
-  // console.log(`Time ${t2 - t1} ms`)
   return {
     props: {
       success: true,
       user,
-      //resumeTableData: resumeData.data,
-      //hasAssignedImages: hasAssignedImages.data,
     },
   }
 }
